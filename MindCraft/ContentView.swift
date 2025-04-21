38392d8bfe00cddea581dd4e6278d55668f94198
nissenemj <nissenemj@gmail.com>
2025-04-21 19:14:14 +0300

    Initial Commit

---
+++ MindCraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindCraft
+//
+//  Created by Mette Nissen on 21.4.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindCraft/MindCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindCraftApp.swift
+//  MindCraft
+//
+//  Created by Mette Nissen on 21.4.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
